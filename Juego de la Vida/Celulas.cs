ddd539b7ba52f30e3ebf7dfc880c33bcf8454899
Gabriel <gabriel.mendez0@hotmail.com>
2019-03-13 15:38:08 -0400

    Upload Game

---
+++ Juego de la Vida/Celulas.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_de_la_Vida
+{
+
+    public class Celulas
+    {
+
+        public int sizeX { get; set; }
+        public int sizeY { get; set; }
+        bool[,] RamdonCuadrado;
+        bool[,] MiCuadrado;
+
+        public Celulas(int x, int y)
+        {
+                this.MiCuadrado = new bool[x, y];
+            this.RamdonCuadrado = new bool[x, y];
+
+        }
+
+        //MATRIZ RAMDON
+        
+        public void GenerarMatriz()
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    Console.Write("|");
+                    if (this.RamdonCuadrado[x, y])
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                        Console.Write(" ");
+                }
+
+                Console.WriteLine("|");
+            }
+        }
+        public void RamdomMatriz()
+        {
+            Random Aleatorio = new Random();
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    this.RamdonCuadrado[x, y] = Aleatorio.NextDouble() < 0.2;
+                }
+            }
+        }       
+        
+        public int SiExiste(int x, int y)
+        {
+            if (x < 0 || x > sizeX - 1)
+                return 0;
+            if (y < 0 || y > sizeY - 1)
+                return 0;
+
+            if (this.RamdonCuadrado[x, y])
+            {
+                return 1;
+            }
+            else return 0;
+        }
+
+        public int Proceso(int x, int y)
+        {
+            return
+                SiExiste(x, y + 1) +
+                SiExiste(x, y - 1) +
+                SiExiste(x + 1, y) +
+                SiExiste(x - 1, y) +
+                SiExiste(x + 1, y + 1) +
+                SiExiste(x + 1, y - 1) +
+                SiExiste(x - 1, y + 1) +
+                SiExiste(x - 1, y - 1);
+        }
+        public void Avanzar()
+        {
+            bool[,] Almaceno = new bool[sizeX, sizeY];
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    Almaceno[x, y] = this.CasiFin(x, y);
+                }
+
+            }
+            this.RamdonCuadrado = Almaceno;
+        }
+        public bool CasiFin(int x, int y)
+        {
+            bool vivo = this.RamdonCuadrado[x, y];
+            int CelularAlrededos = this.Proceso(x, y);
+            if (!vivo)
+            {
+                if (CelularAlrededos == 3)
+                {
+                    return true;
+                }
+            }
+            if (vivo)
+            {
+                if (CelularAlrededos == 2 || CelularAlrededos == 3)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Final()
+        {
+            string Fin = "";
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    Fin += "|";
+                    if (RamdonCuadrado[x, y])
+                    {
+                        Fin += "X";
+                    }
+                    else
+                        Fin += " ";
+
+                }
+                Fin += "|\n";
+            }
+            Console.WriteLine(Fin);
+
+        }
+        // MI MATRIZ
+        public void GenerarMiMatriz ()
+        {
+            int x = 1;
+            Console.WriteLine("Cuanta celula quiere viva?");
+           int vivos = Convert.ToInt32(Console.ReadLine());
+            while ( x <= vivos){
+                Console.WriteLine("Esta es la celula: " + x + " \nIntroduzca la coordenada de x: ");
+                int.TryParse(Console.ReadLine(), out int PosX);
+                Console.WriteLine("Introduzca la coordenada de y: ");
+                int.TryParse(Console.ReadLine(), out int PosY);
+                if (PosX < 0 || PosX >= sizeX || PosY < 0 || PosY >= sizeY)
+                {
+                    Console.WriteLine("No es valido. Introduzca de nuevo.");
+                    continue;
+                }else
+
+                MiCuadrado[PosX, PosY] = true;
+                x++;
+
+                Console.WriteLine();
+            }
+            
+        }
+
+        public int SiExisteMiMatriz(int x, int y)
+        {
+            if (x < 0 || x > sizeX - 1)
+                return 0;
+            if (y < 0 || y > sizeY - 1)
+                return 0;
+
+            if (this.MiCuadrado[x, y])
+            {
+                return 1;
+            }
+            else return 0;
+        }
+
+        public int ProcesoMiMatriz(int x, int y)
+        {
+            return
+                SiExisteMiMatriz(x, y + 1) +
+                SiExisteMiMatriz(x, y - 1) +
+                SiExisteMiMatriz(x + 1, y) +
+                SiExisteMiMatriz(x - 1, y) +
+                SiExisteMiMatriz(x + 1, y + 1) +
+                SiExisteMiMatriz(x + 1, y - 1) +
+                SiExisteMiMatriz(x - 1, y + 1) +
+                SiExisteMiMatriz(x - 1, y - 1);
+        }
+
+        public void AvanzarMiMatriz()
+        {
+            bool[,] AlmacenoMio = new bool[sizeX, sizeY];
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    AlmacenoMio[x, y] = this.CasiFinMiMatriz(x, y);
+                }
+
+            }
+            this.MiCuadrado = AlmacenoMio;
+        }
+
+        public void GenerarMatrizPropia()
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    Console.Write("|");
+                    if (this.MiCuadrado[x, y])
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine("|");
+
+            }
+        }
+
+        public bool CasiFinMiMatriz(int x, int y)
+        {
+            bool vivo = this.MiCuadrado[x, y];
+            int CelulaAlrededor = this.ProcesoMiMatriz(x, y);
+            if (!vivo)
+            {
+                if (CelulaAlrededor == 3)
+                {
+                    return true;
+                }
+            }
+            if (vivo)
+            {
+                if (CelulaAlrededor == 2 || CelulaAlrededor == 3)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void FinalMiMatriz()
+        {
+            string Quitar = "";
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    Quitar += "|";
+                    if (MiCuadrado[x, y])
+                    {
+                        Quitar += "X";
+                    }
+                    else
+                        Quitar += " ";
+
+                }
+                Quitar += "|\n";
+            }
+            Console.WriteLine(Quitar);
+        }
+
+    }
+}
---
+++ Juego de la Vida/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Juego_de_la_Vida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el # Columnas: ");
+            int.TryParse(Console.ReadLine(), out int x);
+            if (x <= 0)
+            {
+                Console.WriteLine("Caracter no valido.");
+            }
+            else
+                Console.WriteLine("Introduzca el # Filas: ");
+
+            int.TryParse(Console.ReadLine(), out int y);
+            Celulas Matriz = new Celulas(x, y);
+            Matriz.sizeX = x;
+            Matriz.sizeY = y;
+            if (y <= 0)
+            {
+                Console.WriteLine("Caracter no valido.");
+            }
+            else
+            {
+
+
+                Console.WriteLine("¿Que desea hacer?");
+                Console.WriteLine("1.Generar Matriz aleatoria.\n2.Generar tu propia Matriz.\n3.Salir");
+                int.TryParse(Console.ReadLine(), out int opcion);
+              
+                switch (opcion)
+                {
+                    case 1:
+                        bool seguir = true;
+                        Console.WriteLine("Esta es su Matriz: ");
+                        Matriz.RamdomMatriz();
+                        Matriz.GenerarMatriz();
+                        while (seguir)
+                        {
+                            Console.WriteLine("Deseas ejecutar el programa automaticamente? \n1.Si \n2.No");
+                            string automatic = Console.ReadLine();
+                            if (automatic == "Si" || automatic == "1" || automatic == "si")
+                            {
+                                Console.WriteLine("Cuanta veces quieres que se ejecute? ");
+                                int number;
+                                int a = 0;
+                                int.TryParse(Console.ReadLine(), out number);
+                                Console.Clear();
+                                if (number == 0)
+                                {
+                                    Console.WriteLine("Caracter no valido.");
+                                }
+                                while (a < number)
+                                {
+                                    Thread.Sleep(250);
+                                    Console.Clear();
+                                    Matriz.Avanzar();
+                                    Matriz.Final();
+                                    Console.WriteLine("Esta es la Ejecucion #" + (a + 1) + ".");
+                                    a++;
+                                }
+                                Console.WriteLine("Gracias por Utilizarme :v");
+                                break;
+
+
+                            }
+                            else if (automatic == "No" || automatic == "2" || automatic == "no")
+                            {
+
+                                Console.WriteLine("Presione ENTER para continuar!");
+                                Console.ReadLine();
+                                while (true)
+                                {
+                                    //Console.Clear();
+                                    Matriz.Avanzar();
+                                    Matriz.Final();
+
+                                    Console.WriteLine("Presione ENTER para continuar, escriba 'Salir' o 'Exit' para Salir. ");
+                                    string outt = Console.ReadLine();
+                                    if (outt == "Salir" || outt == "Exit" || outt == "salir" || outt == "exit")
+                                    {
+                                        seguir = false;
+                                        Console.WriteLine("Gracias por utilizarme! :v");
+                                        break;
+                                    }
+
+                                }
+
+                            }
+                            else Console.WriteLine("Caracter no valido!");
+                            continue;
+                        }
+                        break;
+
+                    case 2:
+                        bool seguir2 = true;
+                        Matriz.GenerarMiMatriz();
+                        Console.WriteLine("Esta es su Matriz: ");
+                        Matriz.GenerarMatrizPropia();
+                        while (seguir2)
+                        {
+                            Console.WriteLine("Deseas ejecutar el programa automaticamente? \n1.Si \n2.No");
+                            string automatic = Console.ReadLine();
+                            if (automatic == "Si" || automatic == "1" || automatic == "si")
+                            {
+                                Console.WriteLine("Cuanta veces quieres que se ejecute? ");
+                                int number;
+                                int a = 0;
+                                int.TryParse(Console.ReadLine(), out number);
+                                Console.Clear();
+                                if (number == 0)
+                                {
+                                    Console.WriteLine("Caracter no valido.");
+                                }
+                                while (a < number)
+                                {
+                                    Thread.Sleep(250);
+                                    Console.Clear();
+                                    Matriz.AvanzarMiMatriz();
+                                    Matriz.FinalMiMatriz();
+                                    Console.WriteLine("Esta es la Ejecucion #" + (a + 1) + ".");
+                                    a++;
+                                }
+                                Console.WriteLine("Gracias por Utilizarme :v");
+                                break;
+
+
+                            }
+                            else if (automatic == "No" || automatic == "2" || automatic == "no")
+                            {
+
+                                Console.WriteLine("Presione ENTER para continuar!");
+                                Console.ReadLine();
+                                while (true)
+                                {
+                                    //Console.Clear();
+                                    Matriz.AvanzarMiMatriz();
+                                    Matriz.FinalMiMatriz();
+
+                                    Console.WriteLine("Presione ENTER para continuar, escriba 'Salir' o 'Exit' para Salir. ");
+                                    string outt = Console.ReadLine();
+                                    if (outt == "Salir" || outt == "Exit" || outt == "salir" || outt == "exit")
+                                    {
+                                        seguir2 = false;
+                                        Console.WriteLine("Gracias por utilizarme! :v");
+                                        break;
+                                    }
+
+                                }
+
+                            }
+                            else Console.WriteLine("Caracter no valido!");
+                            continue;
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Pase buen dia! :3");
+                        break;
+
+                    default:
+                        Console.WriteLine("Se ha introducido caracter invalido.");
+                        break;
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
